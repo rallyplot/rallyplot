09535854ea99fcf7b8490b496dc05bcad2e4bcb5
rallyplot <rallyplot@gmail.com>
2025-12-21 23:56:04 +0000

    Last go on windows.

--- tests/python/test_full_plotter.py
+++ tests/python/test_full_plotter.py
@@ -253,28 +253,28 @@ class TestPlotter:
 
         self._handle_check(plotter, "test_anti_aliasing_1")
         plotter.finish()
-        plotter = None
 
-        print(
-            "Anti-alias at 2. The next plot will be at 8 and should be improved."
-        )
+        if False:
 
-        plotter = Plotter(anti_aliasing_samples=2, color_mode="light")
-        plotter.line(open, width=5)
+            print(
+                "Anti-alias at 2. The next plot will be at 8 and should be improved."
+            )
 
-        self._handle_check(plotter, "test_anti_aliasing_2")
-        plotter.finish()
-        plotter = None
+            plotter = Plotter(anti_aliasing_samples=2, color_mode="light")
+            plotter.line(open, width=5)
 
-        print(
-            "Anti-alias at 4."
-        )
-        plotter = Plotter(anti_aliasing_samples=32, color_mode="light")
-        plotter.line(open, width=5)
+            self._handle_check(plotter, "test_anti_aliasing_2")
+            plotter.finish()
 
-        self._handle_check(plotter, "test_anti_aliasing_3")
-        plotter.finish()
-        plotter = None
+            print(
+                "Anti-alias at 4."
+            )
+            plotter = Plotter(anti_aliasing_samples=32, color_mode="light")
+            plotter.line(open, width=5)
+
+            self._handle_check(plotter, "test_anti_aliasing_3")
+            plotter.finish()
+
 
     def test_plot_args(self, candlestick_data):
         """anti-aliasing not tested here"""
