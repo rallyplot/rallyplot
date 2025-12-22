36e70b1f469a9921161816dd80832026f11e205b
JoeZiminski <joseph.j.ziminski@gmail.com>
2025-12-22 01:41:57 +0000

    intentionally break again.

--- tests/python/test_full_plotter.py
+++ tests/python/test_full_plotter.py
@@ -82,7 +82,7 @@ class TestPlotter:
             open[i] = close[i - 1]
             close[i] = open[i] + diffs[i]
 
-        low = close - np.random.uniform(0, 0.5)
+        low = close - np.random.uniform(0, 0.5) * 10
         high = open + np.random.uniform(0, 0.5)
 
         return (open, high, low, close)
