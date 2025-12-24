d020f044f053746c9118a25e31da74c99f82d8ee
rallyplot <rallyplot@gmail.com>
2025-12-24 04:04:02 +0000

    Centralise a bit more, need to document.

--- distribution/build_wheel.py
+++ distribution/build_wheel.py
@@ -19,6 +19,13 @@ env = os.environ.copy()
 
 os_name = platform.system()
 
+if os_name == "Windows":
+    env["Qt6_DIR"] = f"{qt_install_path.as_posix()}/6.8.2/msvc2022_64/lib/cmake/Qt6"
+elif os_name == "Darwin":
+    env["Qt6_DIR"] = f"{qt_install_path.as_posix()}/6.7.3/macos/lib/cmake/Qt6"
+elif os_name == "Linux":
+    env["Qt6_DIR"] = f"{qt_install_path.as_posix()}/6.8.2/gcc_64/lib/cmake/Qt6"
+
 env["CMAKE_ARGS"] = "-DRALLYPLOT_BUILD_DEV=ON -DUSE_VENDORED_QT=ON"
 
 subprocess.run(
